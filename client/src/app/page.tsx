6ce93c3328765b27545d58cb1c17d3d0265324ef
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-10-01 17:06:50 +0200

    Implemented necessary config

---
+++ client/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import AboutUs from "@/components/AboutUs";
+import Offers from "@/components/Offers";
+import Experiences from "@/components/Experiences";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Services />
+      <AboutUs />
+      <Offers />
+      <Experiences />
+    </div>
+  );
+}
---
+++ client/src/components/Services/Services.tsx
@@ -0,0 +1,55 @@
+import { bebas_neue } from "@/app/font";
+import styles from "./Services.module.css";
+import Image from "next/image";
+
+export default function Services() {
+  const services = [
+    {
+      icon: "/images/pipe-image.png",
+      label: "Odgušivanje cevi",
+    },
+    {
+      icon: "/images/warning-image.png",
+      label: "Hitne intervencije",
+    },
+    {
+      icon: "/images/wrench-image.png",
+      label: "Popravke",
+    },
+    {
+      icon: "/images/sink-image.png",
+      label: "Zamena sanitarije",
+    },
+    {
+      icon: "/images/camera-image.png",
+      label: "Termalno snimanje",
+    },
+  ];
+
+  return (
+    <div className={`${styles.servicesWrapper} container`}>
+      <h3 className={`${styles.servicesHeading} ${bebas_neue.className}`}>
+        ODGUŠIVANJE CEVI U NOVOM SADU
+      </h3>
+      <div className={styles.servicesContainer}>
+        {services.map((service, index) => (
+          <div className={styles.service} key={index}>
+            <div className={styles.serviceCircle}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={service.icon}
+                  width={120}
+                  height={120}
+                  alt="Pipe image"
+                />
+              </div>
+              <div className={styles.topHalf} />
+              <div className={styles.bottomHalf} />
+            </div>
+            <p className={styles.serviceDescription}>{service.label}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
