cc01f88ca733129f81ccf52dd8666ebe6cd628a4
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-10-18 11:03:57 +0200

    Returned to gtag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,31 +22,29 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          id="gtm-script"
+          id="google-analytics"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer' ? '&l='+l : '';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KK36G6DC');`,
-          }}
+          src="https://www.googletagmanager.com/gtag/js?id=G-GHKCYEYPP3"
         />
+        <Script id="google-analytics-config" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GHKCYEYPP3');
+            gtag('config', 'AW-16742086901');
+          `}
+        </Script>
       </head>
       <body className={montserrat.className}>
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KK36G6DC"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
+        </noscript> */}
 
         {children}
       </body>
