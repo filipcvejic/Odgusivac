6b3fda1c9db2c29f63ff3fabdc6ecf38b8a5f768
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2025-02-06 00:13:29 +0100

    Updated phone number

--- client/src/contants.ts
+++ client/src/contants.ts
@@ -1,4 +1,4 @@
 export const EMAIL = "vodooinstalatermica@gmail.com";
-export const PHONE_NUMBER = "0628253911";
+export const PHONE_NUMBER = "0658602548";
 export const ADDRESS = "Temerinska 25";
 export const PHONE_HREF_LOCATION = `tel:${PHONE_NUMBER}`;
